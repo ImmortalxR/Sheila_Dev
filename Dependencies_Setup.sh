23c799f50a27f2574032e1945ac088b9fd82ae90
Astro <alezcano93@gmail.com>
2018-02-17 18:04:56 -0500

    This commit adds the dependencies shell script to the build.

---
+++ Dependencies_Setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Fetching Dependencies for Sheila.py"
+
+pip install pyttsx
+pip install SpeechRecognition
+pip install weather-api
+
+#Will eventually add section to compile 
+OPENCV2 when facial recognition is added again
