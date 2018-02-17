151533c7879328726c608d345306f10d42276bd6
Astro <alezcano93@gmail.com>
2018-02-17 18:18:35 -0500

    Updated the dependency installer.

--- Dependencies_Setup.sh
+++ Dependencies_Setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 echo "Fetching Dependencies for Sheila.py"
 
+#Primary Dependencies
 pip install pyttsx
 pip install SpeechRecognition
 pip install weather-api
 
+#Secondary Dependencies
+sudo apt install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libav-tools
+pip install pyaudio
+pip install google-api-python-client
+pip install monotonic
+
 #Will eventually add section to compile 
-OPENCV2 when facial recognition is added again
+#OPENCV2 when facial recognition is added again
