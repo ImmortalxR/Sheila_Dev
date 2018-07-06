0f7a0c0d3898c69971b1218282d8a0b8dd0a2aa4
Unknown <a.lezcano93@gmail.com>
2018-07-06 13:23:08 -0400

    More debug additions
    The previous attempt had an error

--- Sheila/Sheila_Core.py
+++ Sheila/Sheila_Core.py
@@ -92,7 +92,7 @@ def Sheila_Core():
                     print("--------------------------------------------------")
                     print("...I heard my name...")
                     print("--------------------------------------------------")
-                    say("I heard you, you said: " + fullVOSplit)
+                    say("I heard you, you said: " + str(inputVO))
                     detFlag = True
                     break
                 elif detFlag is True:
